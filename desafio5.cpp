d1ec9e324f86adee43dac39924e7967fb867ba75
Cauã Antunes <caua.roca@gmail.com>
2017-10-12 23:14:52 -0300

    bugadão

--- desafio5.cpp
+++ desafio5.cpp
@@ -3,71 +3,63 @@
 #include <crypto++/aes.h>
 #include <crypto++/modes.h>
 #include <crypto++/filters.h>
+#include <omp.h>
 using namespace std;
 
 int main(int argc, char* argv[]) {
 
-    byte key[ CryptoPP::AES::DEFAULT_KEYLENGTH ], iv[ CryptoPP::AES::BLOCKSIZE ];
-    key[0] = 0x65; key[1] = 0x73; key[2] = 0x73; key[3] = 0x61; key[4] = 0x73; key[5] = 0x65; key[6] = 0x6e;
-    key[7] = 0x68; key[8] = 0x61; key[9] = 0x65; key[10] = 0x68; key[11] = 0x66; key[12] = 0x72; key[13] = 0x61;
-    key[14] = 0x63; key[15] = 0x61;
-    memset( iv, 0x00, CryptoPP::AES::BLOCKSIZE );
+    byte key[ CryptoPP::AES::DEFAULT_KEYLENGTH ] = {'K', 'e', 'y', '2', 'G', 'r', 'o', 'u', 'p', '1', '4', 'X', 'X', 'X', 'X', 'X'};
 
-    cout << endl << CryptoPP::AES::BLOCKSIZE << endl;
-
-    //
-    // String and Sink setup
-    //
-    string plaintext = "Texto para teste";
-    string ciphertext;
+    string plaintext;
+    string ciphertext = "";
     string decryptedtext;
 
-    //
-    // Dump Plain Text
-    //
-    cout << "Plain Text (" << plaintext.size() << " bytes)" << endl;
-    cout << plaintext;
-    cout << endl << endl;
-
-    //
-    // Create Cipher Text
-    //
-    //CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
-    CryptoPP::ECB_Mode< CryptoPP::AES >::Encryption ecbEncryption;
-    ecbEncryption.SetKey(key,sizeof(key));
-
-    CryptoPP::StreamTransformationFilter stfEncryptor(ecbEncryption, new CryptoPP::StringSink( ciphertext ) );
-    stfEncryptor.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() + 1 );
-    stfEncryptor.MessageEnd();
-
-    //
-    // Dump Cipher Text
-    //
-    cout << "Cipher Text (" << ciphertext.size() << " bytes)" << endl;
-
-    for( int i = 0; i < ciphertext.size(); i++ ) {
-
-        cout << "0x" << hex << (0xFF & static_cast<byte>(ciphertext[i])) << " ";
+    char c;
+    while(cin >> c && ciphertext.size() < 16){
+        if(c >= '0' && c <= '9'){
+            int d = (c - 48)*16;
+            cin >> c;
+            d += c - 48;
+            ciphertext += d;
+	}
     }
 
-    cout << endl << endl;
+    while(ciphertext.size() % 16 != 0) ciphertext += '\0';
 
-    //
-    // Decrypt
-    //
-    CryptoPP::ECB_Mode< CryptoPP::AES >::Decryption ecbDecryption;
-    ecbDecryption.SetKey(key,sizeof(key));
+    cout << ciphertext.size() << endl;
 
-    CryptoPP::StreamTransformationFilter stfDecryptor(ecbDecryption, new CryptoPP::StringSink( decryptedtext ) );
-    stfDecryptor.Put( reinterpret_cast<const unsigned char*>( ciphertext.c_str() ), ciphertext.size() );
-    stfDecryptor.MessageEnd();
+/*    for(key[11] = 33; key[11] <= 126; key[11]++){
+        for(key[12] = 33; key[12] <= 126; key[12]++){
+            for(key[13] = 33; key[13] <= 126; key[13]++){
+                for(key[14] = 33; key[14] <= 126; key[14]++){
+                    #pragma omp parallel for
+                    for(key[15] = 33; key[15] <= 126; key[15]++){
+*/
+                        //
+                        // Decrypt
+                        //
+                        CryptoPP::ECB_Mode< CryptoPP::AES >::Decryption ecbDecryption;
+                        ecbDecryption.SetKey(key,sizeof(key));
 
+			try{
+                        CryptoPP::StreamTransformationFilter stfDecryptor(ecbDecryption, new CryptoPP::StringSink( decryptedtext ) );
+                        stfDecryptor.Put( reinterpret_cast<const unsigned char*>( ciphertext.c_str() ), ciphertext.size() );
+                        stfDecryptor.MessageEnd();
+			} catch(CryptoPP::InvalidCiphertext e){
+				cout << endl << e.GetWhat() << endl;
+			}
     //
     // Dump Decrypted Text
     //
+    cout << "Key: " << key << endl;
     cout << "Decrypted Text: " << endl;
     cout << decryptedtext;
     cout << endl << endl;
-
+/*                    }
+                }
+            }
+        }
+    }
+*/
     return 0;
 }
