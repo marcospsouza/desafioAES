25a64f809ae3dcd19a8db033fe6c7cdca6e2f51e
marcosps96 <marcos_souza96@hotmail.com>
2017-10-16 22:31:59 -0700

    deixei rodando de madrugada, talvez n funcione mas acho q vai

--- desafio.c
+++ desafio.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <omp.h>
+#include <time.h>
 // Enable both ECB and CBC mode. Note this can be done before including aes.h or at compile-time.
 // E.g. with GCC by using the -D flag: gcc -c aes.c -DCBC=0 -DECB=1
 #define CBC 1
@@ -12,20 +13,16 @@
 
 int main(void)
 {
-    uint8_t ciphertext[16] = {0x84, 0xa8, 0x27, 0xb3, 0xe1, 0x8B, 0x85, 0xa9, 0x63, 0x5a, 0x93, 0x58, 0x2c, 0x96, 0xa7, 0x57}
+    uint8_t ciphertext[16] = {0x84, 0xa8, 0x27, 0xb3, 0xe1, 0x8B, 0x85, 0xa9, 0x63, 0x5a, 0x93, 0x58, 0x2c, 0x96, 0xa7, 0x57};
     //uint8_t key[] = {'e', 's', 's', 'a', 's', 'e', 'n', 'h', 'a', 'e', 'h', 'f', 'r', 'a', 'c', 'a'};
     uint8_t key[] = {'K', 'e', 'y', '2', 'G', 'r', 'o', 'u', 'p', '1', '4', 'X', 'X', 'X', 'X', 'X'};
     uint8_t decryptedtext[16];
 
-   
-    int i;
-    for(i = 0; i < 16; i++){
-      //ciphertext[i] = fgetc(f);
-      printf("%c", ciphertext[i]);
-    }
+       
     printf("\nCOMEÇANDO\n");
 
 
+    clock_t begin_time = clock();
     for(key[11] = 33; key[11] <= 126; key[11]++){
         for(key[12] = 33; key[12] <= 126; key[12]++){
             for(key[13] = 33; key[13] <= 126; key[13]++){
@@ -39,14 +36,17 @@ int main(void)
                         AES_ECB_decrypt(ciphertext, key, decryptedtext, 16);
                 
                         int alpha = 0;
-                        for(int i = 0; i < 16; i++){
-                            if ((decryptedtext[i] < 48 || decryptedtext[i] > 57) && (decryptedtext[i] < 65 || decryptedtext[i] > 90) && (decryptedtext[i] < 97 || decryptedtext[i] > 122) && decryptedtext[i] != 10 && decryptedtext[i] != 'á' && decryptedtext[i] != 'à' && decryptedtext[i] != 'ã' && decryptedtext[i] != 'â' && decryptedtext[i] != 'é' && decryptedtext[i] != 'ê' && decryptedtext[i] != 'ú' && decryptedtext[i] != 'í' && decryptedtext[i] != 'ç' && decryptedtext[i] != 'Á' && decryptedtext[i] != 'À' && decryptedtext[i] != 'Ã' && decryptedtext[i] != 'Â' && decryptedtext[i] != 'É' && decryptedtext[i] != 'Ê' && decryptedtext[i] != 'Ú' && decryptedtext[i] != 'Í' && decryptedtext[i] != 'Ç' && decryptedtext[i] != ',' && decryptedtext[i] != '.' && decryptedtext[i] != ' '){
+                         for(int i = 0; i < 16; i++){
+                            if( (decryptedtext[i] >= 65 && decryptedtext[i] <= 90) || (decryptedtext[i] >= 97 && decryptedtext[i] <= 122) 
+                                || (decryptedtext[i] == 32)|| decryptedtext[i] == 'á' || decryptedtext[i] == 'à' || decryptedtext[i] == 'ã' 
+                                || decryptedtext[i] == 'â' || decryptedtext[i] == 'é' || decryptedtext[i] == 'ê' || decryptedtext[i] == 'ú' 
+                                || decryptedtext[i] == 'í' || decryptedtext[i] == 'ç' || decryptedtext[i] == 'Á' || decryptedtext[i] == 'À' 
+                                || decryptedtext[i] == 'Ã' || decryptedtext[i] == 'Â' || decryptedtext[i] == 'É' || decryptedtext[i] == 'Ê' 
+                                || decryptedtext[i] == 'Ú' || decryptedtext[i] == 'Í' || decryptedtext[i] == 'Ç' )
                                 alpha++;
-                            }
-
                         }
                         
-                        if(alpha < 2){
+                        if(alpha >= 14){
                             //
                             // Dump Decrypted Text
                             //
@@ -54,7 +54,8 @@ int main(void)
                             sprintf(name, "%d.%d.%d.%d.%d.txt", key[11], key[12], key[13], key[14], key[15]);
                            
                             FILE *log_file = fopen(name, "ab+");
- 
+                            
+                            fprintf(log_file, "Time: %f\n", ((float) (clock() - begin_time))/CLOCKS_PER_SEC);
                             fprintf(log_file, "KEY: ");
                             for(int i = 0; i < 16; i++){
                                 fprintf(log_file, "%c", key[i]);
